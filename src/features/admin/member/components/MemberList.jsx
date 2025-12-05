5153afa9d6b19de4dd46aa0058a6859df75dae00
DarkLight0418 <samseong0418@naver.com>
2025-12-05 09:06:47 +0900

    날짜 유틸 함수 붙여넣기

--- src/features/admin/member/components/MemberList.jsx
+++ src/features/admin/member/components/MemberList.jsx
@@ -1,3 +1,18 @@
 export default function MemberList() {
 
+
+  // [0] 날짜 포맷 함수
+  function formatDate(dateString) {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    return date.toLocaleString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    });
+  }
 }
