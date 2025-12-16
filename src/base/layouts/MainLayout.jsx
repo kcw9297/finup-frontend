c3989575e3da1bd90135ad23ec097213715a57d0
rmadudbb <rmadudbb@gmail.com>
2025-12-16 17:20:56 +0900

    종목상세 배경색 변경

--- src/base/layouts/MainLayout.jsx
+++ src/base/layouts/MainLayout.jsx
@@ -1,18 +1,22 @@
 import { Box } from "@mui/material";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import thema from "../../base/design/thema.js";
+import { useMatch } from 'react-router-dom';
 
 /**
  * 기본적으로 사용하는 메인 레이아웃
  */
 export default function MainLayout({ children }) {
+   const isDetailPage = useMatch('/stocks/detail/:code');
+
 
   return (
     // 외부 박스 영역 (높이 결정 가능)
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', ...(isDetailPage && { backgroundColor: thema.palette.background.light }) }}>
 
       {/* 헤더 영역 */}
-      <Header />
+      <Header sx={{...(isDetailPage && { backgroundColor: thema.palette.background.light }) }}/>
 
       {/* 중앙 메인 영역 (너비 관리) */}
       <Box
@@ -31,7 +35,7 @@ export default function MainLayout({ children }) {
       </Box>
 
       {/* 푸터 영역 */}
-      <Footer />
+      <Footer sx={{...(isDetailPage && { backgroundColor: thema.palette.background.light }) }}/>
     </Box>
   )
 }
