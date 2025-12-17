895dfed846b3dd32beff69be230083ff9f5447bc
rmadudbb <rmadudbb@gmail.com>
2025-12-17 17:17:55 +0900

    퀴즈 권한

--- src/features/quiz/hooks/useQuiz.js
+++ src/features/quiz/hooks/useQuiz.js
@@ -5,20 +5,22 @@ export function useQuiz(){
   
   // [1] 필요 데이터 선언 
   const [quiz, setQuiz] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   // [2] 필요 함수 선언 //없음 생략
   // [3] 성공/실패 콜백 함수 정의 //없음 생략
 
   // [4] REST API 요청 함수 정의 
   useEffect(()=>{
+    if (!open) return; 
+    setLoading(true);
     async function fetchQuiz() {
-      const rp = await api.get(`/quiz/getQuestion`, { public: true });
+      const rp = await api.get(`/quiz/getQuestion`);
       setQuiz(rp.data);
       setLoading(false);
     }
     fetchQuiz();
-  }, []);
+  }, [open]);
   // [5] 반환
   return { quiz, loading };
 }
